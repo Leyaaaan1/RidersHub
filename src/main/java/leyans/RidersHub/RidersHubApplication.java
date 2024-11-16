7041e149f786676849b348264b4d002a86c4311b
Leandro Paninsoro <paninsorolean@gmail.com>
2024-11-16 23:48:52 +0800

    First commit

---
+++ src/main/java/leyans/RidersHub/RidersHubApplication.java
@@ -0,0 +1,14 @@
+package leyans.RidersHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidersHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidersHubApplication.class, args);
+	}
+
+
+}
