c3e2faf1baf82467a77fcb962b77d7fa87f33b68
Leandro Paninsoro <paninsorolean@gmail.com>
2024-12-23 08:18:54 +0800

    add authenticationManager

--- src/main/java/leyans/RidersHub/SecurityConfig/SecurityConfiguration.java
+++ src/main/java/leyans/RidersHub/SecurityConfig/SecurityConfiguration.java
@@ -1,4 +1,44 @@
 package leyans.RidersHub.SecurityConfig;
 
+import leyans.RidersHub.Service.userDetailsManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final userDetailsManager userDetailsManager;
+
+    public SecurityConfiguration(leyans.RidersHub.Service.userDetailsManager userDetailsManager) {
+        this.userDetailsManager = userDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityConfiguration(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager autmanager(HttpSecurity  httpSecurity) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                httpSecurity.getSharedObjects(AuthenticationManagerBuilder.class);
+
+
+    }
+
 }
