a5eafe5a973953461ba7ca01405c6594e78977e4
Leandro Paninsoro <paninsorolean@gmail.com>
2024-12-23 10:45:48 +0800

    add login controller

---
+++ src/main/java/leyans/RidersHub/Controller/loginController.java
@@ -0,0 +1,30 @@
+package leyans.RidersHub.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class loginController {
+
+    public final AuthenticationManager authenticationManager;
+
+    public loginController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authenticationRequest
+    }
+
+
+
+
+
+
+}
+
