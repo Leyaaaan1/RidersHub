2c08cf0879e6ac9a6ef7837396923b408b694a30
Leandro Paninsoro <paninsorolean@gmail.com>
2024-12-26 23:43:38 +0800

    add login form with connected database, but just plain text encoder yet

--- src/main/java/leyans/RidersHub/Controller/loginController.java
+++ src/main/java/leyans/RidersHub/Controller/loginController.java
@@ -1,7 +1,7 @@
 package leyans.RidersHub.Controller;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +17,22 @@ public class loginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest) {
-        Authentication authenticationRequest
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authenticationRequest = UsernamePasswordAuthenticationToken
+                .unauthenticated(
+                        loginRequest.username(),
+                        loginRequest.password());
+        Authentication authenticationResponse =
+                this.authenticationManager.authenticate(authenticationRequest);
+
+        return ResponseEntity.ok("Login successful");
     }
 
 
+
+    public record LoginRequest(String username, String password) {
+
+    }
 
 
 
