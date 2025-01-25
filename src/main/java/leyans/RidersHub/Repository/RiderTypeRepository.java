02368801e04d8cb06d19dceee7c5d34f0642a66c
Leandro Paninsoro <paninsorolean@gmail.com>
2025-01-25 09:11:11 +0800

    add role base authentication using different types of rides, add DTO for the endpoints

---
+++ src/main/java/leyans/RidersHub/Repository/RiderTypeRepository.java
@@ -0,0 +1,12 @@
+package leyans.RidersHub.Repository;
+
+import leyans.RidersHub.model.RiderType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RiderTypeRepository extends JpaRepository<RiderType, Integer> {
+    RiderType findByRiderType(String riderType);
+
+
+}
--- src/main/java/leyans/RidersHub/SecurityConfig/SecurityConfiguration.java
+++ src/main/java/leyans/RidersHub/SecurityConfig/SecurityConfiguration.java
@@ -16,19 +16,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
 import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
-
 import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.COOKIES;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
-
-
-
-
-
-    // Renamed the method to avoid conflicts
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -37,28 +29,25 @@ public class SecurityConfiguration {
                         .addLogoutHandler(new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(COOKIES))))
                 .sessionManagement(session -> session
                         .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::newSession))
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("rider/update").authenticated()
-                        .requestMatchers("/rider/add").hasRole("Admin").anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/rider/all").hasRole("CAR")
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
 
-
         return httpSecurity.build();
     }
-
     @Bean
     public AuthenticationManager authenticationManager(
-            UserDetailsManager userDetailsManager, PasswordEncoder passwordEncoder) {
+            UserDetailsManager userDetailsManager) {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsManager);
         authenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-
         return new ProviderManager(authenticationProvider);
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
-
 }
