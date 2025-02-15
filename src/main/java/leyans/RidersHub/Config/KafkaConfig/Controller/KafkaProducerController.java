4301d7d7434b0b8edb71ddff9e11dcf45341c18b
Leandro Paninsoro <paninsorolean@gmail.com>
2025-02-16 06:40:09 +0800

    adding feature to kafka, sending message to each ridertypes

--- src/main/java/leyans/RidersHub/Config/KafkaConfig/Controller/KafkaProducerController.java
+++ src/main/java/leyans/RidersHub/Config/KafkaConfig/Controller/KafkaProducerController.java
@@ -4,21 +4,42 @@ package leyans.RidersHub.Config.KafkaConfig.Controller;
 import leyans.RidersHub.Config.KafkaConfig.Service.KafkaProducerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/kafka")
 public class KafkaProducerController {
 
     @Autowired
     private KafkaProducerService kafkaProducerService;
 
-    @GetMapping("/send")
-    public void sendMessage(@RequestParam("message") String message) {
-        kafkaProducerService.sendMessage(message);
+    @GetMapping("/sendCar")
+    public void sendMessage(
+            @RequestParam("riderType") String riderType,
+            @RequestParam("message") String message) {
+
+        kafkaProducerService.sendMessage(riderType, message);
+
     }
+
+    @GetMapping("/sendMotor")
+    public void sendMotor(
+            @RequestParam("riderType") String riderType,
+            @RequestParam("message") String message) {
+
+        kafkaProducerService.sendMotor(riderType, message);
+
+    }
+
+    @GetMapping("/sendBike")
+    public void sendBike(
+            @RequestParam("riderType") String riderType,
+            @RequestParam("message") String message) {
+
+        kafkaProducerService.sendBike(riderType, message);
+
+    }
+
 
 
 
