ae6b5c39e0150b6d10055502a6382a50032e1edf
Leyaaan1 <paninsorolean@gmail.com.com>
2025-02-18 18:33:00 +0800

    add temporary locations entities with service and controller

---
+++ src/main/java/leyans/RidersHub/DTO/LocationRequest.java
@@ -0,0 +1,32 @@
+package leyans.RidersHub.DTO;
+
+public class LocationRequest {
+
+    private String location;
+    private String latitude;
+    private String longitude;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
--- src/main/java/leyans/RidersHub/model/Location.java
+++ src/main/java/leyans/RidersHub/model/Location.java
@@ -12,21 +12,76 @@ public class Location {
     @Column(name = "locationId", unique = true, nullable = false)
     private Integer locationId;
 
-    @Column(columnDefinition = "geometry(Point, 4325)")
-    private Point location;
+   // @Column(columnDefinition = "geometry(Point, 4325)")
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "username", referencedColumnName = "username", nullable = false)
     private Rider usernamae;
 
-    
+
     @Column(name = "location", nullable = true)
     private String locationName;
 
-    private static final GeometryFactory geo = new GeometryFactory();
+    @Column(name = "latitude", nullable = false)
+    private String latitud;
+
+    @Column(name = "longtitude", nullable = false)
+    private String longtitude;
+
+    public Location(Integer locationId, Rider usernamae, String locationName, String latitud, String longtitude) {
+        this.locationId = locationId;
+        this.usernamae = usernamae;
+        this.locationName = locationName;
+        this.latitud = latitud;
+        this.longtitude = longtitude;
+    }
+
+    public Location() {
+
+    }
+    //private static final GeometryFactory geo = new GeometryFactory();
+
+
+    public Integer getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Integer locationId) {
+        this.locationId = locationId;
+    }
 
 
 
+    public Rider getUsernamae() {
+        return usernamae;
+    }
 
+    public void setUsernamae(Rider usernamae) {
+        this.usernamae = usernamae;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getLongtitude() {
+        return longtitude;
+    }
+
+    public void setLongtitude(String longtitude) {
+        this.longtitude = longtitude;
+    }
 }
 
