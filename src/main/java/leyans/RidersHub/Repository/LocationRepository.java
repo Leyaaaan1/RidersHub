125593359aac57ee7d22a8b7f9522d5b44e39930
leyaaaan1 <paninsorolean@gmail.com>
2025-03-09 19:19:06 +0800

    the distance calculations is fixed, remove the table for update location and move it to the kafka, add both reponseDTO for adding new rides and locations update

--- src/main/java/leyans/RidersHub/Repository/LocationRepository.java
+++ src/main/java/leyans/RidersHub/Repository/LocationRepository.java
@@ -1,18 +1,36 @@
 package leyans.RidersHub.Repository;
 
+import jakarta.transaction.Transactional;
 import leyans.RidersHub.model.Dynamic.Locations;
 import leyans.RidersHub.model.Rider;
 import leyans.RidersHub.model.Rides;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Locations, Integer> {
 
 
+
+    @Modifying
+    @Transactional
+    @Query("INSERT INTO Rides (locationName, ridesName, username, startingPoint, endingPoint, date, latitude, longitude) " +
+            "VALUES (:locationName, :ridesName, :username,  :startingPoint, :endingPoint, :date, :latitude, :longitude)")
+    void saveSelectedFields(
+            @Param("locationName") String locationName,
+            @Param("ridesName") String ridesName,
+            @Param("username") Rider username,
+            @Param("startingPoint") String startingPoint,
+            @Param("endingPoint") String endingPoint,
+            @Param("date") LocalDateTime date,
+            @Param("latitude") Double latitude,
+            @Param("longitude") Double longitude
+    );
 //Locations findByUsername(String username);
 }
