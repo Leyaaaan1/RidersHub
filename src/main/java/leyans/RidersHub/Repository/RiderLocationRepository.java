a52561a719fcc1246d4f9a081b2108a0f32d8cd3
leyaaaan1 <paninsorolean@gmai.com>
2025-05-25 13:37:23 +0800

    add the custom haversine focmula

--- src/main/java/leyans/RidersHub/Repository/RiderLocationRepository.java
+++ src/main/java/leyans/RidersHub/Repository/RiderLocationRepository.java
@@ -1,6 +1,7 @@
 package leyans.RidersHub.Repository;
 
 import leyans.RidersHub.model.RiderLocation;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,9 +9,7 @@ import org.springframework.data.repository.query.Param;
 public interface RiderLocationRepository extends JpaRepository<RiderLocation, Integer> {
 
     // Uses PostGIS ST_DistanceSphere for geodesic distance in meters
-    @Query(value = "SELECT ST_DistanceSphere(rl.location, ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)) " +
-            "FROM rider_locations rl WHERE rl.id = :locId", nativeQuery = true)
-    double findDistanceMeters(@Param("locId") Integer locId,
-                              @Param("lat") double latitude,
-                              @Param("lon") double longitude);
+    @Query(value = "SELECT ST_DistanceSphere(:pointA, :pointB)", nativeQuery = true)
+    double getDistanceBetweenPoints(@Param("pointA") Point pointA, @Param("pointB") Point pointB);
+
 }
