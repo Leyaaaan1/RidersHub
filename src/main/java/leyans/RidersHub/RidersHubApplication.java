1ddf82b2a7fc51f58f97380f24c38ad0ae446694
Leyaaaan1 <paninsorolean@gmail.com>
2025-05-29 17:02:13 +0800

    add minor changes

--- src/main/java/leyans/RidersHub/RidersHubApplication.java
+++ src/main/java/leyans/RidersHub/RidersHubApplication.java
@@ -1,5 +1,6 @@
 package leyans.RidersHub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import leyans.RidersHub.Config.JWT.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class RidersHubApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(RidersHubApplication.class, args);
 
 	}
