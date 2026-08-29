698d48fccbdb2a9f1d85f939eaf2ab7aea9e7584
Leandro Paninsoro <paninsorolean@gmail.com>
2026-08-29 16:52:53 +0800

    add new endpoints for n8n (error detection)

--- backend/src/main/java/leyans/RidersHub/Utility/RestTemplateConfig.java
+++ backend/src/main/java/leyans/RidersHub/Utility/RestTemplateConfig.java
@@ -3,6 +3,7 @@ package leyans.RidersHub.Utility;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,20 +12,26 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
-    @Bean
-    public RestTemplate restTemplate() {
-        RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectTimeout(Timeout.ofSeconds(10))
-                .setResponseTimeout(Timeout.ofSeconds(45))
-                .build();
+        @Bean
+        public RestTemplate restTemplate() {
+                RequestConfig requestConfig = RequestConfig.custom()
+                                .setConnectTimeout(Timeout.ofSeconds(10))
+                                .setResponseTimeout(Timeout.ofSeconds(45))
+                                .build();
 
-        HttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(requestConfig)
-                .build();
+                PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+                // Re-validate a pooled connection if it's been idle for >1s before reuse
+                connectionManager.setValidateAfterInactivity(org.apache.hc.core5.util.TimeValue.ofSeconds(1));
 
-        HttpComponentsClientHttpRequestFactory factory =
-                new HttpComponentsClientHttpRequestFactory(httpClient);
+                HttpClient httpClient = HttpClientBuilder.create()
+                                .setConnectionManager(connectionManager)
+                                .setDefaultRequestConfig(requestConfig)
+                                .evictIdleConnections(org.apache.hc.core5.util.TimeValue.ofSeconds(30))
+                                .evictExpiredConnections()
+                                .build();
 
-        return new RestTemplate(factory);
-    }
+                HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+                return new RestTemplate(factory);
+        }
 }
